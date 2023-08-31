57b36a3d8141fdaa6a34111fb2f15c10bf8fa80e
Leeuw <mohamedessabir20@gmail.com>
2023-08-31 20:46:47 +0100

    update backend

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, UploadFile, File
-import os
-import vedo
-from model import predict , predict_alpha
 from helpers import create_temp_file, delete_temp_file
+from fastapi import FastAPI, UploadFile, File
+from model import predict, predict_alpha
 from config import AppConfig
+
+import json
+import vedo
+import os
 
 
 app = FastAPI()
@@ -13,7 +15,7 @@ config = AppConfig(app)
 
 @app.get("/")
 def read_root():
-    return {"message": "3DSF Interns 2023 - Group 2!"}
+    return {"message": "TeethSeg MeshSegNet API by 3DSF Interns!"}
 
 
 @app.post("/api/v1/predict")
@@ -41,18 +43,27 @@ async def predict_and_send(file: UploadFile = File(...)):
         fileNameWithExt = out_filename + ".vtp"
 
         # Send the prediction result file to the frontend
-        with open(os.path.join(config.OUTPUT_FOLDER, fileNameWithExt), "rb") as out_file:
+        with open(os.path.join(config.OUTPUT_FOLDER, fileNameWithExt), "r") as out_file:
             prediction_file_data = out_file.read()
 
+        prediction = {
+            "filename": fileNameWithExt,
+            "prediction_file": prediction_file_data
+        }
+
         # Delete the temporary file
-        os.remove(temp_filepath)
+        try :
+            os.remove(temp_filepath)
+        except FileNotFoundError as e:
+            pass 
 
         # Delete files in the OUTPUT folder
         delete_temp_file(config.OUTPUT_FOLDER, out_filename)
 
         return {
-            "filename": fileNameWithExt,
-            "prediction_file": prediction_file_data
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps(prediction)
         }
 
     except Exception as e:
@@ -89,19 +100,28 @@ async def predict_and_sendPalpha(file: UploadFile = File(...)):
         fileNameWithExt = out_filename[1] + ".vtp"
 
         # Send the prediction result file to the frontend
-        with open(os.path.join(config.OUTPUT_FOLDER, fileNameWithExt), "rb") as out_file:
+        with open(os.path.join(config.OUTPUT_FOLDER, fileNameWithExt), "r") as out_file:
             prediction_file_data = out_file.read()
         
+        prediction = {
+            "filename": fileNameWithExt,
+            "prediction_file": prediction_file_data
+        }
+
         # Delete the temporary file
-        os.remove(temp_filepath)
+        try :
+            os.remove(temp_filepath)
+        except FileNotFoundError as e:
+            pass 
         
         # Delete files in the OUTPUT folder
         for i in range(3):
             delete_temp_file(config.OUTPUT_FOLDER, out_filename[i])
 
         return {
-            "filename": fileNameWithExt,
-            "prediction_file": prediction_file_data
+            'statusCode': 200,
+            'headers': {'Content-Type': 'application/json'},
+            'body': json.dumps(prediction)
         }
     
     except Exception as e:
