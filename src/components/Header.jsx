838469e6d8bda6af2b0508ad593e6a5321930b81
ABDELILAH <77416814+BrightProgrammer7@users.noreply.github.com>
2023-08-31 04:45:41 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,7 +26,7 @@ function Header() {
           }}
         >
           <Menu />
-          <div className="bg-background text-foreground  absolute top-20 left-0 flex-box flex-col pb-8 w-full z-100">
+          <div className="bg-background text-foreground  absolute top-20 left-0 flex-box flex-col pb-8 w-full z-99">
             {menuOpen ? (
               <>
                 {" "}
