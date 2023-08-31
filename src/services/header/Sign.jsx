d52f79c417ea114b6952236a9d7c392f0893777e
Hamza <hamza.lbelghiti@gmail.com>
2023-08-31 12:35:53 +0100

    fixed colors

--- src/services/header/Sign.jsx
+++ src/services/header/Sign.jsx
@@ -52,10 +52,10 @@ function Sign() {
 			) : (
 				<>
 					<button
-						className="relative inline-flex items-center justify-center mb-2 mr-2 p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-blue-500 to-green-400 group-hover:from-blue-500 group-hover:to-gren-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 hover:shadow-lg  hover:scale-105 transition-all duration-300 ease-in-out"
+						className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800"
 						onClick={() => handleLogOut()}
 					>
-						<span className="flex-box text-base font-semibold  transition ease-linear leading-tight relative px-6 py-3 bg-background text-foreground rounded items-start gap-3 group-hover:bg-opacity-0 whitespace-nowrap">
+						<span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
 							Log Out
 						</span>
 					</button>
